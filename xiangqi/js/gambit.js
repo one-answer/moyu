957094e6882a433dbca16545b160733aafa6c894
Oliver <wqdyxnbd@163.com>
2025-03-04 15:44:44 +0800

    Add games yxlm

---
+++ xiangqi/js/gambit.js
@@ -0,0 +1,4 @@
+/*! 摸鱼小游戏 | qq:1836360247 | 欢迎指教 */
+com.gambit = com.gambit ||
+	""
+	.split(" ")
---
+++ xiaokonglongmaoxian/spilGamesWrapper.js
@@ -0,0 +1,28 @@
+﻿var GameWrapper = {
+    isAsking: false,
+    askAds: function (x) {
+        if (!GameWrapper.isAsking) {
+            GameWrapper.isAsking = true;
+            if (GameAPI.IS_STANDALONE) {               
+                GameWrapper.onResume();
+            } else {
+                GameAPI.GameBreak.request(function () {
+                    GameWrapper.onPause();
+                }, function () {
+                    GameWrapper.onResume();
+                });
+            }
+
+        }
+
+    },
+    onResume: function () {
+        GameWrapper.isAsking = false;
+        $("#funcName").val("OnResume");
+        $("#exeCommand").click();
+    },
+    onPause: function () {
+        $("#funcName").val("OnPaused");
+        $("#exeCommand").click();
+    },
+};
